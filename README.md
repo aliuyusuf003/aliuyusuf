d228b2929bd2386decd6644c6184966561f3c4ea
Aliu Yusuf <yusufgenius03@gmail.com>
2021-09-24 12:33:49 +0100

    Create Skill Resource

---
+++ README.md
@@ -0,0 +1,6 @@
+## Command Lists
+
+
+- [php artisan make:model Skill --migration
+- [php artisan make:controller SkillController --resource
+- [php artisan route:list
---
+++ app/Http/Controllers/SkillController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Skill;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Skill::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'percentage'=> 'required'
+        ]);
+        return Skill::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Skill::find($id);
+    }
+
+    /**
+     * Search the specified resource.
+     *
+     * @param  int  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Skill::where('name','like','%'.$name.'%')->get();
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       
+        $skill = Skill::find($id);
+        $skill->update($request->all());
+        
+        return $skill;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Skill::destroy($id);
+         
+    }
+}
